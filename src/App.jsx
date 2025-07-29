4c6b9e7d407d9c9e2ea6727f79c1806793031203
Dmitri Karpatchev <dmitri@Dmitris-Mini.localdomain>
2025-07-29 22:28:24 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+
+export default function App() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>Solana Messenger</h1>
+      <p>Прототип для отправки сообщения через Memo</p>
+    </div>
+  );
+}
